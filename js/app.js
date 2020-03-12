d52be79c1edce87a6d8393749386e5a3b0bccf29
Vadzim Liashkevich <lesh1j@yandex.by>
2020-03-12 18:40:21 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,35 +6,35 @@ import AppStore from "./stores/AppStore";
 import { prepareDataTypes } from './utils/prepareData';
 
 export interface GoGraphProps {
-	options?: object;
-	data?: object;
+options?: object;
+data?: object;
 }
 
 class GoGraph extends Component<GoGraphProps, {}>{ 
 	
-	get stores(){
-		const { options, data } = this.props;
+get stores(){
+const { options, data } = this.props;
 
-		const appStore = new AppStore(options, prepareDataTypes(data));
+const appStore = new AppStore(options, prepareDataTypes(data));
 
-		return {
-		  AppStore: appStore, 
-		  GraphStore: appStore.GraphStore,
-		  MenuStore: appStore.MenuStore,
-		  RightMenuStore: appStore.RightMenuStore,
-		  InfoCardStore: appStore.InfoCardStore,
-		  RiskCardStore: appStore.RiskCardStore,
-		  ZoomStore: appStore.ZoomStore
-		};
-	}
+return {
+AppStore: appStore, 
+GraphStore: appStore.GraphStore,
+MenuStore: appStore.MenuStore,
+RightMenuStore: appStore.RightMenuStore,
+InfoCardStore: appStore.InfoCardStore,
+RiskCardStore: appStore.RiskCardStore,
+ZoomStore: appStore.ZoomStore
+};
+}
 
-	render(){
-		return(
-			<Provider {...this.stores} >
-				<App />
-			</Provider>
-		)
-	}
+render(){
+return(
+<Provider {...this.stores} >
+<App />
+</Provider>
+)
+}
 }
 
 export default GoGraph;
