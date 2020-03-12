5286d299d42bf8babe7703aeb4acfb30b7812ae3
Vadzim Liashkevich <lesh1j@yandex.by>
2020-03-12 18:46:26 +0200

    Add files via upload

---
+++ js/index.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import { Provider } from "mobx-react";
+import App from "./components/app";
+import AppStore from "./stores/AppStore";
+import { prepareDataTypes } from './utils/prepareData';
+
+import data from './utils/data';
+const options = {
+	"theme": "dark",
+	"direction": 0
+}
+
+export interface GoGraphProps {
+	options?: object;
+	data?: object;
+}
+
+class GoGraph extends Component<GoGraphProps, {}>{
+	
+	get stores(){
+		const { options, data } = this.props;
+
+		const appStore = new AppStore(options, prepareDataTypes(data));
+
+		return {
+		  AppStore: appStore, 
+		  GraphStore: appStore.GraphStore,
+		  MenuStore: appStore.MenuStore,
+		  RightMenuStore: appStore.RightMenuStore,
+		  InfoCardStore: appStore.InfoCardStore,
+		  RiskCardStore: appStore.RiskCardStore,
+		  ZoomStore: appStore.ZoomStore
+		};
+	}
+
+	render(){
+		return(
+			<Provider {...this.stores} >
+				<App />
+			</Provider>
+		)
+	}
+}
+
+ReactDOM.render((
+	<GoGraph options={options} data={data}/>
+), document.getElementById('root'));
+
+export default GoGraph;
