d02c7ef3ec650a0be6ff4b23201ae57ec6f97679
Vadzim Liashkevich <lesh1j@yandex.by>
2020-08-06 10:27:02 +0300

    Add files via upload

---
+++ gograph/precache-manifest.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0442852c67e82e9f08dbd9e6b00ec3a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "658adadc823bd37ac509",
+    "url": "./static/css/2.chunk.css"
+  },
+  {
+    "revision": "d675ff74c250c9c9e155",
+    "url": "./static/css/main.chunk.css"
+  },
+  {
+    "revision": "658adadc823bd37ac509",
+    "url": "./static/js/2.chunk.js"
+  },
+  {
+    "revision": "06580c76a01f445002de1299f55ec6f6",
+    "url": "./static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d675ff74c250c9c9e155",
+    "url": "./static/js/main.chunk.js"
+  },
+  {
+    "revision": "0b27cc1d036300614753",
+    "url": "./static/js/runtime-main.js"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "./static/media/Roboto.ttf"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "./static/media/Roboto.woff"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "./static/media/Roboto.woff2"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "./static/media/Robotomedium.ttf"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "./static/media/Robotomedium.woff"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "./static/media/Robotomedium.woff2"
+  },
+  {
+    "revision": "a654b49e599b26649c903c0f321bb0cc",
+    "url": "./static/media/icon_rmenu_9.png"
+  },
+  {
+    "revision": "9d3953914f2ebcd34789a0a33057ebf7",
+    "url": "./static/media/icon_rmenu_9_dark.png"
+  }
+]);
