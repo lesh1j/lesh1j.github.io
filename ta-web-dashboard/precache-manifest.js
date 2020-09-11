7027af3fa1476e3c45d1f3f12d2d76862872bc7b
Vadzim Liashkevich <lesh1j@yandex.by>
2020-09-11 12:58:00 +0300

    Add files via upload

---
+++ ta-web-dashboard/precache-manifest.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782303f156f03a8a7dbfb284ce0c68ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ed7d3dffad47a9c3769",
+    "url": "./static/css/main.chunk.css"
+  },
+  {
+    "revision": "96733ac7721ce88445e7",
+    "url": "./static/js/2.chunk.js"
+  },
+  {
+    "revision": "110fa9fb44de74f91c7da0c357ac4a1f",
+    "url": "./static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ed7d3dffad47a9c3769",
+    "url": "./static/js/main.chunk.js"
+  },
+  {
+    "revision": "6b1f9edb53028271aa40",
+    "url": "./static/js/runtime-main.js"
+  },
+  {
+    "revision": "f6bf60c0f3c179bcca820972d5949b46",
+    "url": "./static/media/arrow.svg"
+  },
+  {
+    "revision": "c0fea760e14394c03f86835bb90eb080",
+    "url": "./static/media/back.svg"
+  },
+  {
+    "revision": "9224df033d20d2e2c1b727b11ea77434",
+    "url": "./static/media/circulation.svg"
+  },
+  {
+    "revision": "3e921aba2b20141e6e41967aca1fe740",
+    "url": "./static/media/dark.svg"
+  },
+  {
+    "revision": "867ef858d013a600d75e8e090ca63bc0",
+    "url": "./static/media/dropdown.svg"
+  },
+  {
+    "revision": "f9fda1c8c331ac753b09d29738fa4760",
+    "url": "./static/media/icons.svg"
+  },
+  {
+    "revision": "28b86f55c545808e338658294675152f",
+    "url": "./static/media/info.svg"
+  },
+  {
+    "revision": "5c6f1f24f46738985dae34a12271d234",
+    "url": "./static/media/kn.svg"
+  },
+  {
+    "revision": "9a88d4b07d39d24c03c7387ab02b66db",
+    "url": "./static/media/light.svg"
+  },
+  {
+    "revision": "f2ac4c93317c2b1a632b7449352bc009",
+    "url": "./static/media/list.svg"
+  },
+  {
+    "revision": "31bfed02ec9b65d154c41128fc2062b5",
+    "url": "./static/media/logo.svg"
+  },
+  {
+    "revision": "0c9516a8899a184780f792af96369c57",
+    "url": "./static/media/mainBackground.png"
+  },
+  {
+    "revision": "34ec90c73f4f1017f20039a7b1f11cd3",
+    "url": "./static/media/map.svg"
+  },
+  {
+    "revision": "b71f1104e22637d0c4340b2d645f3986",
+    "url": "./static/media/mapBg.png"
+  },
+  {
+    "revision": "ad9e603520ec9b7fd55c5650387ef721",
+    "url": "./static/media/resales.svg"
+  },
+  {
+    "revision": "d5fc9323d21440ed48f78e67b7e21728",
+    "url": "./static/media/restoreFilters.svg"
+  },
+  {
+    "revision": "11afaa5e53a70b33d2d68fa95be9fa50",
+    "url": "./static/media/search.svg"
+  },
+  {
+    "revision": "b2ae43953f4cc0a7693047cdaaf996c8",
+    "url": "./static/media/traceability.svg"
+  }
+]);
