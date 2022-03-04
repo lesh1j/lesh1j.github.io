ad8070d3abcb0cb66ac8c76099f9905d1bdbf92b
Vadzim Liashkevich <lesh1j@yandex.by>
2022-03-04 17:53:37 +0300

    Add files via upload

---
+++ build/precache-manifest.0f1bf71e49f0965f9fb4b6e327308b11.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8077897b68afb8cd33d8402c96797e42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d22ce25e40026ddfceba",
+    "url": "./static/css/2.chunk.css"
+  },
+  {
+    "revision": "44da882ddaaf9d30240f",
+    "url": "./static/css/main.chunk.css"
+  },
+  {
+    "revision": "d22ce25e40026ddfceba",
+    "url": "./static/js/2.chunk.js"
+  },
+  {
+    "revision": "f2ea367afaefd2e55c7ebe8a60b5b30a",
+    "url": "./static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44da882ddaaf9d30240f",
+    "url": "./static/js/main.chunk.js"
+  },
+  {
+    "revision": "1a339ac1417664a7b4d77ab14605f018",
+    "url": "./static/js/main.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b27cc1d036300614753",
+    "url": "./static/js/runtime-main.js"
+  },
+  {
+    "revision": "613dc8fe0671ab7b29148f6a030a337c",
+    "url": "./static/media/accepted.svg"
+  },
+  {
+    "revision": "1862ff15237e8e28c4744af0190be17d",
+    "url": "./static/media/actual.svg"
+  },
+  {
+    "revision": "72532c179c7296b7b740dcdd24515e4f",
+    "url": "./static/media/angle.svg"
+  },
+  {
+    "revision": "c7a6472aef13d2e2fd2020928668f19d",
+    "url": "./static/media/arrow.svg"
+  },
+  {
+    "revision": "bb22fdff53085f002365a002a24974d1",
+    "url": "./static/media/arrowLeft.svg"
+  },
+  {
+    "revision": "7aaff712de30151a0690561d90e04c5f",
+    "url": "./static/media/arrowRight.svg"
+  },
+  {
+    "revision": "44b2ce17d19e95f96370fd20c5b1e1a5",
+    "url": "./static/media/check.svg"
+  },
+  {
+    "revision": "a69d95a9aa5b750c3b05369235262c55",
+    "url": "./static/media/dots.svg"
+  },
+  {
+    "revision": "ca29f833fafc3a81fff4f99fb31b8de9",
+    "url": "./static/media/download.svg"
+  },
+  {
+    "revision": "c5bf3e6215c7f20539633f28d1454357",
+    "url": "./static/media/error.svg"
+  },
+  {
+    "revision": "594b73abf49a042ca2db0631689b4f58",
+    "url": "./static/media/errorA01.svg"
+  },
+  {
+    "revision": "da271b9d886c55f9da6878415a5852f3",
+    "url": "./static/media/errorData.svg"
+  },
+  {
+    "revision": "e8d56bbc5af91fb728736fb4f81a2a32",
+    "url": "./static/media/excluded.svg"
+  },
+  {
+    "revision": "78ebdd854d81b2d68996d5256a040cf4",
+    "url": "./static/media/hasError.svg"
+  },
+  {
+    "revision": "3709a65e56739f1399a1896b099f6b27",
+    "url": "./static/media/icon_rmenu_13.gif"
+  },
+  {
+    "revision": "a05606183fc24c0af7de2ae839a4cca9",
+    "url": "./static/media/icon_rmenu_13_dark.gif"
+  },
+  {
+    "revision": "a654b49e599b26649c903c0f321bb0cc",
+    "url": "./static/media/icon_rmenu_9.png"
+  },
+  {
+    "revision": "9d3953914f2ebcd34789a0a33057ebf7",
+    "url": "./static/media/icon_rmenu_9_dark.png"
+  },
+  {
+    "revision": "d8275b286c139803debd92ff7d74b80c",
+    "url": "./static/media/logofns.svg"
+  },
+  {
+    "revision": "e7d26e6f903baa3941386bcca44a6d92",
+    "url": "./static/media/noError.svg"
+  },
+  {
+    "revision": "b75170221c8f983603fbc6ce6ba82d5d",
+    "url": "./static/media/not_actual.svg"
+  },
+  {
+    "revision": "eedc327fc2f8ba55298950cf5e83f98a",
+    "url": "./static/media/open.svg"
+  },
+  {
+    "revision": "c8f8a1bfdb8dadb660714af330065350",
+    "url": "./static/media/print.svg"
+  },
+  {
+    "revision": "ba2bcb669bd38b819039a0d4afcf4bfa",
+    "url": "./static/media/processed.svg"
+  },
+  {
+    "revision": "7b23f59d84ba9231b12741f0f4461a83",
+    "url": "./static/media/ready.svg"
+  },
+  {
+    "revision": "b5d3c8e61c146a333330ada734237e82",
+    "url": "./static/media/search.svg"
+  },
+  {
+    "revision": "c389d30b414e59fc01b1cef725152180",
+    "url": "./static/media/sendError.svg"
+  },
+  {
+    "revision": "4725bd76de6255896d725e41063cba50",
+    "url": "./static/media/sendOk.svg"
+  },
+  {
+    "revision": "fce4dbcc2b2d3f41571f7e9f8c2b6784",
+    "url": "./static/media/sort.svg"
+  },
+  {
+    "revision": "8790bbbede2214b91b710f523fc9fb28",
+    "url": "./static/media/system.svg"
+  },
+  {
+    "revision": "1ef9a0b97b0644d0825e7bc37b9eec44",
+    "url": "./static/media/transferred.svg"
+  },
+  {
+    "revision": "dd0c0f73c9f040e23570f8d7fe29de49",
+    "url": "./static/media/transferredA01.svg"
+  },
+  {
+    "revision": "49567be295b052febf5aff3921a7f284",
+    "url": "./static/media/user.svg"
+  }
+]);
