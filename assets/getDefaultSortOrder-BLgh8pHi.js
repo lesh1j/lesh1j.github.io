36b601d23d1c015b841a7fbe7925daceee819687
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-09-11 11:51:22 +0300

    sad

---
+++ assets/getDefaultSortOrder-BLgh8pHi.js
@@ -0,0 +1,2 @@
+import{E as o,a}from"./Table-Do4TyD6y.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
+//# sourceMappingURL=getDefaultSortOrder-BLgh8pHi.js.map
--- assets/getDefaultSortOrder-DAwv9O44.js
+++
@@ -1,2 +0,0 @@
-import{E as o,a}from"./Table-CihpQGrf.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
-//# sourceMappingURL=getDefaultSortOrder-DAwv9O44.js.map
