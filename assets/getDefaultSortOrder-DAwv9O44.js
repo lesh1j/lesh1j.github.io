8e1ac1b3e86f72d5757abf2b3248cc6899a60a56
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-09-11 11:42:53 +0300

    loader

---
+++ assets/getDefaultSortOrder-DAwv9O44.js
@@ -0,0 +1,2 @@
+import{E as o,a}from"./Table-CihpQGrf.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
+//# sourceMappingURL=getDefaultSortOrder-DAwv9O44.js.map
