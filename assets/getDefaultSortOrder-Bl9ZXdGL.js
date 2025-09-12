e6f2e19ae0c609afc278aa963f02fef12cc87204
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-09-12 11:57:54 +0300

    asd

--- assets/getDefaultSortOrder-Bl9ZXdGL.js
+++
@@ -1,2 +0,0 @@
-import{E as o,a}from"./Table-CCgFMl5e.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
-//# sourceMappingURL=getDefaultSortOrder-Bl9ZXdGL.js.map
---
+++ assets/getDefaultSortOrder-DfDia2-R.js
@@ -0,0 +1,2 @@
+import{E as o,a}from"./Table-zC1y0uQH.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
+//# sourceMappingURL=getDefaultSortOrder-DfDia2-R.js.map
