48c85c66aaa3d909fc80e67486134615adb6aa94
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-09-18 18:20:27 +0300

    casc

---
+++ assets/getDefaultSortOrder-DLx1KUHL.js
@@ -0,0 +1,2 @@
+import{E as o,a}from"./Table-CN9oHtcp.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
+//# sourceMappingURL=getDefaultSortOrder-DLx1KUHL.js.map
