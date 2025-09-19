663160e7349b843ce6eb078322181eed4a8ce2a6
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-09-19 13:13:44 +0300

    asd

--- assets/getDefaultSortOrder-i1Pj9ssk.js
+++
@@ -1,2 +0,0 @@
-import{E as o,a}from"./Table-BxXC8_Ym.js";const E=(r,s)=>{const t=r.get(o.ORDERING),e=t!=null&&t.startsWith("-")?a.DESC:a.ASC;return t!=null&&t.endsWith(s)?e:void 0};export{E as g};
-//# sourceMappingURL=getDefaultSortOrder-i1Pj9ssk.js.map
