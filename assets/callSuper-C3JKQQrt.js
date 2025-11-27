fa2f67194a3020633758dce107a79c4214b6b03a
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-11-27 22:47:37 +0300

    zxcxzc

---
+++ assets/callSuper-C3JKQQrt.js
@@ -0,0 +1,2 @@
+import{dL as c,dr as s,dM as o}from"./index-B62SLujc.js";function u(r,t,e){return t=s(t),c(r,o()?Reflect.construct(t,e||[],s(r).constructor):t.apply(r,e))}export{u as _};
+//# sourceMappingURL=callSuper-C3JKQQrt.js.map
---
+++ assets/useNotFoundError-DAH2QNGy.js
@@ -0,0 +1,2 @@
+import{v as u,r as i}from"./index-B62SLujc.js";const c=t=>{const s=u.c(3),o=t&&"originalStatus"in t?t.originalStatus:t&&"status"in t?t.status:-1;let a,n;s[0]!==o?(a=()=>{if(o===404)throw new Response("Not Found",{status:404})},n=[o],s[0]=o,s[1]=a,s[2]=n):(a=s[1],n=s[2]),i.useEffect(a,n)};export{c as u};
+//# sourceMappingURL=useNotFoundError-DAH2QNGy.js.map
