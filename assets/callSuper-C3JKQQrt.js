ed23369f6070b1d52f2f80ec658a7aecf0ebaa26
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-11-27 22:51:28 +0300

    ASDAS

--- assets/callSuper-C3JKQQrt.js
+++
@@ -1,2 +0,0 @@
-import{dL as c,dr as s,dM as o}from"./index-B62SLujc.js";function u(r,t,e){return t=s(t),c(r,o()?Reflect.construct(t,e||[],s(r).constructor):t.apply(r,e))}export{u as _};
-//# sourceMappingURL=callSuper-C3JKQQrt.js.map
---
+++ assets/formatNumber-BGDOij3b.js
@@ -0,0 +1,2 @@
+import{dX as e,dY as a}from"./index-7iycTks3.js";const t=r=>isNaN(Number(r))?(console.error(`formatNumber: The passed string "${r}" is not a number`),null):Number(r).toLocaleString(e[a.language]);export{t as f};
+//# sourceMappingURL=formatNumber-BGDOij3b.js.map
