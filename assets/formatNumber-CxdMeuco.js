670c7e9cdd158accd3682664babae680f337853d
Vadzim Liashkevich <v.liashkevich@mitgo.com>
2025-12-23 17:01:43 +0300

    oo

--- assets/formatNumber-CxdMeuco.js
+++
@@ -1,2 +0,0 @@
-import{cM as e,cN as a}from"./index-BZtEQsN6.js";const t=r=>isNaN(Number(r))?(console.error(`formatNumber: The passed string "${r}" is not a number`),null):Number(r).toLocaleString(e[a.language]);export{t as f};
-//# sourceMappingURL=formatNumber-CxdMeuco.js.map
